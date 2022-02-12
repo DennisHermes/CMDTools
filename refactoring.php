5f3b5debb880f814453cde2ad68c2d573a245433
Dennis Hermes <78319503+DennisHermes@users.noreply.github.com>
2022-02-12 13:38:59 +0100

    * Working on site

---
+++ refactoring.php
@@ -0,0 +1,12 @@
+<?php
+
+    $file = $_FILES['file'];
+
+    $dir = "threath";
+    mkdir($dir, 0700);
+
+    move_uploaded_file($file['tmp_name'], $dir."/".$file['name']);
+
+    header("location: fileReader.php?dir=".$dir."&name=".$file['name']);
+
+?>
